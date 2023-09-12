cd1001485678a71744f701b620269af147ecf711
Andrej Fellinger <fellinger.andrej2gmail.com>
2023-09-12 17:34:12 +0200

    update

--- VUE/src/App.vue
+++ VUE/src/App.vue
@@ -1,30 +1,63 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section>
+    <h1>Nova polozka</h1>
+    <section class="inputs bg">
+      <p>Nazov polozky</p>
+      <input v-model="input" placeholder="vajce">
+      <button @click="pridatItem()">pridat</button>
+    </section>
+
+    <section class="aktualne bg">
+      <h2>Polozky</h2>
+      <ul>
+        <li v-for="polozka in polozky" >{{polozka}}</li>
+      </ul>
+    </section>
+
+    <section class="zmazane bg">
+      <h2>Zmazane</h2>
+      <ul>
+        <li v-for="zmazana in zmazane">{{zmazana}}</li>
+      </ul>
+    </section>
+  </section>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      input: '',
+      polozky: [null],
+      zmazane: [null]
+    }
+  },
+  methods: {
+    pridatItem() {
+      this.polozky.push({
+        text: this.input,
+        id: this.polozky.length + 1,
+        is_deleted: false
+        })
+      this.input = ''
+    }
+  },
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .inputs {
+    display: grid;
+  }
+
+  .inputs button {
+    background: green;
+  }
+
+  .bg{
+    background: rgb(23, 23, 23);
+    border-radius: 3px;
+    padding: 10px;
+    margin: 4px;
+  }
 </style>
